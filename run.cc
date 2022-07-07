6d659535b21b20bc4d882e5ec2d4029937f94fd6
tlatorre <tlatorre@uchicago.edu>
2022-07-07 12:16:30 -0700

    update

--- run.cc
+++ run.cc
@@ -1,4 +1,8 @@
 #include "run.hh"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
 
 MyRunAction :: MyRunAction(G4String OutName)
 { // Constructor
@@ -82,6 +86,13 @@ void MyRunAction::BeginOfRunAction(const G4Run* run)
     G4int runID = run->GetRunID();
     std::stringstream strRunID;
     strRunID << runID;
+
+    struct stat st = {0};
+
+    if (stat("Results", &st) == -1) {
+        printf("Creating directory \"Results\" since it doesn't exist.\n");
+        mkdir("Results", 0700);
+    }
 
     // Creation of Output file
     man->OpenFile("./Results/"+OutputName+strRunID.str()+".root");
